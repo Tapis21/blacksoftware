34a33d067d8acf27424b57976a15a605737474e2
Tapis21 <arontutnajera@gmail.com>
2025-06-15 03:25:32 -0500

    organizacion de archivos por lenguaje
    re-ubicación de archivos en sus carpetas adecuadas

--- ingresosie.php
+++ ingresosie.php
@@ -1,3 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+
 <!DOCTYPE html>
 <html lang="es-LA">
 <head>
@@ -9,14 +15,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 
     <!-- Contenido del css de la pagina original -->
-    <link rel="stylesheet" href="ingresosie.css">
+    <link rel="stylesheet" href="css/ingresosie.css">
 </head>
 <body>
     <div class="main-wrapper">
-    <?php include 'menu.php'; ?>
+    <?php include 'php/menu.php'; ?>
 
     <!-- Footer -->
-    <?php include 'footer.php'; ?>
+    <?php include 'php/footer.php'; ?>
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
